d190275e1088a11f51c97edca80204569795d7b8
WoodManGitHub <cheeseman0930@gmail.com>
2019-05-10 08:34:24 +0000

    add category url

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('post/<int:pk>/', views.detail, name='detail'),
 	re_path('archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/', views.archives, name='archives'),
+	path('category/<int:pk>/', views.category, name='category')
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category
 from django.utils.html import strip_tags
 import markdown
 
@@ -9,6 +9,11 @@ def index(request):
 
 def archives(request, year, month):
 	post_list = Post.objects.filter(created_time__year=year, created_time__month=month).order_by('-created_time')
+	return render(request, 'blogapp/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+	cate = get_object_or_404(Category, pk=pk)
+	post_list = Post.objects.filter(category=cate).order_by('-created_time')
 	return render(request, 'blogapp/index.html', context={'post_list': post_list})
 
 def detail(request, pk):
