ecf3e776675b7c187cae582499b614ad7641db9e
WoodManGitHub <cheeseman0930@gmail.com>
2019-05-11 12:41:12 +0000

    fix space and tabs

--- blogapp/templatetags/blogtags.py
+++ blogapp/templatetags/blogtags.py
@@ -5,12 +5,12 @@ register = template.Library()
 
 @register.simple_tag
 def get_recent_posts(num=5):
-	return Post.objects.all().order_by('-created_time')[:num]
+    return Post.objects.all().order_by('-created_time')[:num]
 
 @register.simple_tag
 def archives():
-	return Post.objects.dates('created_time', 'month', order='DESC')
+    return Post.objects.dates('created_time', 'month', order='DESC')
 
 @register.simple_tag
 def get_categories():
-	return Category.objects.all()
+    return Category.objects.all()
--- blogapp/views.py
+++ blogapp/views.py
@@ -4,24 +4,24 @@ from django.utils.html import strip_tags
 import markdown
 
 def index(request):
-	post_list = Post.objects.all()
-	return render(request, 'blogapp/index.html', context={'post_list': post_list})
+    post_list = Post.objects.all()
+    return render(request, 'blogapp/index.html', context={'post_list': post_list})
 
 def archives(request, year, month):
-	post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
-	return render(request, 'blogapp/index.html', context={'post_list': post_list})
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
+    return render(request, 'blogapp/index.html', context={'post_list': post_list})
 
 def category(request, pk):
-	cate = get_object_or_404(Category, pk=pk)
-	post_list = Post.objects.filter(category=cate)
-	return render(request, 'blogapp/index.html', context={'post_list': post_list})
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blogapp/index.html', context={'post_list': post_list})
 
 def detail(request, pk):
-	post = get_object_or_404(Post, pk=pk)
-	post.content = markdown.markdown(strip_tags(post.content),
-									extensions=[
-										'markdown.extensions.extra',
-										'markdown.extensions.codehilite',
-										'markdown.extensions.toc',
-									], safe_mode=True)
-	return render(request, 'blogapp/detail.html', context={'post': post})
+    post = get_object_or_404(Post, pk=pk)
+    post.content = markdown.markdown(strip_tags(post.content),
+				extensions=[
+					'markdown.extensions.extra',
+					'markdown.extensions.codehilite',
+        				'markdown.extensions.toc',
+				], safe_mode=True)
+    return render(request, 'blogapp/detail.html', context={'post': post})
